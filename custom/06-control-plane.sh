662c29cd645e5e16c9e10288644695d33f71e3f8
Amim Knabben <amim.knabben@gmail.com>
2020-11-11 21:15:49 -0500

    node rbac

--- custom/06-control-plane.sh
+++ custom/06-control-plane.sh
@@ -130,4 +130,44 @@ EOF
 
   gcloud compute ssh ${instance} -- "sudo systemctl restart nginx && sudo systemctl enable nginx"
   gcloud compute ssh ${instance} -- kubectl get componentstatuses --kubeconfig admin.kubeconfig
+
+  gcloud compute ssh ${instance} -- cat <<EOF | gcloud compute ssh ${instance} --  kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:kube-apiserver-to-kubelet
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes/proxy
+      - nodes/stats
+      - nodes/log
+      - nodes/spec
+      - nodes/metrics
+    verbs:
+      - "*"
+EOF
+ 
+ gcloud compute ssh ${instance} -- cat <<EOF | gcloud compute ssh ${instance} -- kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
+
+
 done
